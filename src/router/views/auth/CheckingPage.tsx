2703ae2887e61c2677db11370109cd48febed80b
CESCO <cescovalledev@gmail.com>
2024-06-10 16:19:23 -0600

    slight change in checking page

--- src/router/views/auth/CheckingPage.tsx
+++ src/router/views/auth/CheckingPage.tsx
@@ -2,8 +2,8 @@ import { ImSpinner8 } from "react-icons/im";
 
 export const CheckingPage = () => {
   return (
-    <div className="flex justify-center w-full h-screen align-middle bg-background text-foreground">
-      <ImSpinner8 className="w-16 h-16 my-auto animate-spin" />
+    <div className="flex justify-center w-full h-screen align-middle bg-background">
+      <ImSpinner8 className="w-16 h-16 my-auto animate-spin text-foreground" />
     </div>
   );
 };
