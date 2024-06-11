a1d201d47fad6055a5bce6333635f3c1c2db6592
CESCO <cescovalledev@gmail.com>
2024-06-10 22:54:47 -0600

    added avatar and dropdown

--- src/router/views/todo/Layout.tsx
+++ src/router/views/todo/Layout.tsx
@@ -5,10 +5,21 @@ import { useUiStore } from "@/hooks/useUiStore";
 import { useAuthStore } from "@/hooks/useAuthStore";
 
 import { IoSunnySharp, IoMoonSharp } from "react-icons/io5";
-import { CiLogout } from "react-icons/ci";
+import { CiLogout, CiUser } from "react-icons/ci";
 // import { HTML5Backend } from "react-dnd-html5-backend";
 import { TouchBackend } from "react-dnd-touch-backend";
 import { DndProvider } from "react-dnd";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { IRootState } from "@/store";
+import { useSelector } from "react-redux";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const options = {
   enableMouseEvents: true,
@@ -16,9 +27,27 @@ const options = {
   delayTouchStart: 500,
 };
 
+const getInitials = (displayName: string) => {
+  const words = displayName.split(" ");
+  if (words.length === 0) {
+    return "";
+  }
+  let initials = "";
+  for (let i = 0; i < Math.min(2, words.length); i++) {
+    if (words[i].length > 0) {
+      initials += words[i][0].toUpperCase();
+    }
+  }
+
+  return initials;
+};
+
 export const LayoutTodo = () => {
   const { theme, toggleTheme } = useUiStore();
   const { startLogout } = useAuthStore();
+  const { photoURL, displayName } = useSelector(
+    (state: IRootState) => state.auth
+  );
 
   return (
     <>
@@ -44,9 +73,35 @@ export const LayoutTodo = () => {
                   <IoMoonSharp className="w-6 h-6 text-foreground" />
                 )}
               </Button>
-              <Button onClick={() => startLogout()} variant={"link"}>
-                <CiLogout className="w-6 h-6 text-foreground" />
-              </Button>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <Avatar>
+                    <AvatarImage src={photoURL || ""} />
+                    <AvatarFallback>{getInitials(displayName!)}</AvatarFallback>
+                  </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="max-w-52">
+                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>
+                    <Button className="flex w-full gap-3 justify-stretch">
+                      <CiUser className="w-6 h-6 text-primary-foreground" />
+                      <p className="overflow-hidden text-ellipsis">
+                        {displayName}
+                      </p>
+                    </Button>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <Button
+                      className="flex w-full gap-3 justify-stretch"
+                      onClick={() => startLogout()}
+                    >
+                      <CiLogout className="w-6 h-6 text-primary-foreground" />
+                      Logout
+                    </Button>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
             </div>
           </div>
           <DndProvider backend={TouchBackend} options={options}>
